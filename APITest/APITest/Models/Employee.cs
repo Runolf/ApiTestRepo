9dc4c1fb3bb1c0c5d1dfd802d98509932c370f9e
Runolf <38182335+Runolf@users.noreply.github.com>
2020-02-06 16:58:45 +0100

    commit 16h58 06/02/2020

--- APITest/APITest/Models/Employee.cs
+++ APITest/APITest/Models/Employee.cs
@@ -7,6 +7,8 @@ namespace APITest.Models
 {
     public class Employee
     {
+        //https://www.tutorialsteacher.com/webapi/create-web-api-project
+       
         public int ID { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
